fc3a3e4d6aef271842ab654407b605823d2c6109
Darian <badia.noguero@gmail.com>
2024-04-17 00:41:46 +0200

    Arreglat canço en loop del audio manager, events per CharacterAnimations implementat, events per GameManager, Fade script, main menu polit

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -2,49 +2,47 @@
 using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using FMOD.Studio;
+using UnityEngine.Serialization;
 
 public class MainMenu : MonoBehaviour
 {
     [SerializeField] private CanvasGroup _currentCanvas;
+    [SerializeField] private CanvasGroup _blackCanvas;
     [SerializeField] private Animator _cameraAnimator;
-    [SerializeField] private float fadeDuration = 0.5f;
-    [SerializeField] private float fadeDelay = 0.5f;
-
+    [SerializeField] private float _fadeDuration;
+    [SerializeField] private float _fadeDelay;
+    private EventInstance mainMenuSong;
+    private FadePanel fade;
+    
     private void Start()
     {
-        AudioManager.instance.PlayOneShot(AudioManager.instance.MainMenu);
+        fade = GetComponent<FadePanel>();
+        fade.StartCoroutine(fade.Fade(_blackCanvas, 1, 0, 0, _fadeDuration));
+        mainMenuSong = AudioManager.instance.CreateEventInstance(AudioManager.instance.MainMenu);
+        AudioManager.instance.PlayLoopSong(mainMenuSong);
     }
 
     public void StartGame()
     {
-        StartCoroutine(Fade(_currentCanvas, _currentCanvas.alpha, 0, 0));
-        //StopAudio
-        SceneManager.LoadScene("Game");
+        fade.StartCoroutine(fade.Fade(_blackCanvas, 0, 1, 0, _fadeDuration, ChangeScene));
     }
-
+    
     public void ChangeMenu(CanvasGroup newCanvas)
     {
         _currentCanvas.interactable = false;
         _currentCanvas.blocksRaycasts = false;
-        StartCoroutine(Fade(_currentCanvas, _currentCanvas.alpha, 0, 0));
-        StartCoroutine(Fade(newCanvas, _currentCanvas.alpha, 1, fadeDelay));
+        fade.StartCoroutine(fade.Fade(_currentCanvas, _currentCanvas.alpha, 0, 0, _fadeDuration));
+        fade.StartCoroutine(fade.Fade(newCanvas, _currentCanvas.alpha, 1, _fadeDelay, _fadeDuration));
         newCanvas.interactable = true;
         newCanvas.blocksRaycasts = true;
         _currentCanvas = newCanvas;
     }
-    
-    public IEnumerator Fade (CanvasGroup canvas, float start, float end, float delay)
+
+    public void ChangeScene()
     {
-        float counter = 0f;
-        
-        yield return new WaitForSecondsRealtime(delay);
-        
-        while (counter < fadeDuration)
-        {
-            counter += Time.unscaledDeltaTime;
-            canvas.alpha = Mathf.Lerp(start, end, counter / fadeDuration);
-            yield return null;
-        }
+        AudioManager.instance.StopLoopSong(mainMenuSong);
+        SceneManager.LoadScene("CosasDaro");
     }
 
     public void AnimateCamera(string trigger)
